428869b3de9ceeba55c850afcab655ee8789ddfc
Kaben Nanlohy <stochastic@hackamajig.com>
2012-02-08 18:08:14 -0500

    Add example test in C++.

--- src/test/ovd_tests.cmake
+++ src/test/ovd_tests.cmake
@@ -1,6 +1,9 @@
 
 ENABLE_TESTING()
 
+#
+# Python tests.
+#
 set( RAND_PT_CASES 128 256 512 1024 2048 4096 10000 20000)
 foreach( CASE ${RAND_PT_CASES} )
     ADD_TEST(random_points_${CASE} python ../src/test/random_points.py 0 ${CASE})
@@ -41,3 +44,30 @@ endforeach()
 foreach( CASE RANGE 25) # seeds 1..25. These take about 4s each to run.
     ADD_TEST(ttt_random_chars_${CASE} python ../src/test/ttt_random_chars.py ${CASE})
 endforeach()
+
+#
+# C++ tests. Each test subdirectory has its own CMakeLists.txt file with
+# build and test commands. Use "test_sandbox/*" as a template to make a new
+# test subdirectory, like so:
+#
+# $ cp --recursive test_sandbox <test name>_test
+#
+macro(ADD_OVD_TEST test_name libs)
+  # This macro was borrowed and modified from Open CASCADE Community Edition
+  # (http://github.com/tpaviot/oce).
+
+  file(GLOB TEST_SOURCES *.cpp)
+  set(SOURCE_FILES ${TEST_SOURCES})
+  add_executable(${test_name} ${SOURCE_FILES})
+
+  foreach(LIB ${libs})
+     target_link_libraries(${test_name} ${LIB})
+  endforeach(LIB ${libs})
+  target_link_libraries(${test_name} openvoronoi_static ${QD_LIBRARY})
+
+  ADD_TEST(${test_name}  ${test_name})
+endmacro(ADD_OVD_TEST test_name libs)
+
+include_directories(${OpenVoronoi_SOURCE_DIR})
+file(GLOB TEST_SUBDIRS test/*_test test/test_sandbox)
+subdirs(${TEST_SUBDIRS})
